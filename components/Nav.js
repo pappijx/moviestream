f3c4d41e9605482fd1fef29b8be6c61bc13296c0
pappijx <ayush.papani123@gmail.com>
2021-06-26 16:19:00 +0530

    latest changes

--- components/Nav.js
+++ components/Nav.js
@@ -6,20 +6,22 @@ function Nav() {
 
   return (
     <div className="relative">
-      <div className="flex px-10 sm:px-20 text-xl whitespace-nowrap 
-        space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
-        {Object.entries(requests).map(([
-          key,
-          {
-            title,
-            url,
-          },
-        ]) => (
+      <div
+        className="flex px-10 sm:px-20 text-xl whitespace-nowrap 
+        space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide"
+      >
+        {Object.entries(requests).map(([key, { title }]) => (
           <h2
             key={key}
-            onClick={() => router.push(`/?genre=${key}`)}
+            onClick={() => {
+              
+              router.push(`/?genre=${key}`);
+            }}
             className="last:pr-24 cursor-pointer transition duration-100 
-          transform hover:scale-110 hover:text-white active:text-red-500 pb-2">{title}</h2>
+          transform hover:scale-110 hover:text-white active:text-red-500  pb-2 "
+          >
+            {title}
+          </h2>
         ))}
       </div>
       <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12"></div>
@@ -28,4 +30,3 @@ function Nav() {
 }
 
 export default Nav;
-
